d307858fca554dd773d8a9e4eee126a58a8f13c8
sagnikc395 <sagnikchatterjee395@gmail.com>
2025-01-22 01:10:31 +0530

    added changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,3 @@
-<script>
-	import { Button } from '$lib/components/ui/button';
-</script>
-
 <main class="flex min-h-screen flex-col content-center items-center justify-center">
 	<h1 class="text-5xl sm:text-6xl">worklog</h1>
-
-	<Button>Toggle</Button>
 </main>
